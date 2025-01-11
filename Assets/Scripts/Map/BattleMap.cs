7b4b313f0cbf551dd626d1d8b63f7844e441e98d
Flavia Brogle <flavia.brogle@fhnw.ch>
2025-01-11 10:53:12 +0100

    Add relative directive tiles getter to BattleMap

--- Assets/Scripts/Map/BattleMap.cs
+++ Assets/Scripts/Map/BattleMap.cs
@@ -72,6 +72,81 @@ public class BattleMap : IInstance {
         return Mathf.Abs(tileA.Position.x - tileB.Position.x) + Mathf.Abs(tileA.Position.y - tileB.Position.y);
     }
 
+    /// <summary>
+    ///     Get the direction between two tiles, if they are in a straight line.
+    ///     Returns false if the tiles are the same or not in a straight line, with direction set to Up.
+    /// </summary>
+    /// <param name="startTile"> The from tile </param>
+    /// <param name="endTile"> The to tile </param>
+    /// <param name="direction"> The direction between the tiles, or Up if not in a straight line </param>
+    /// <returns> Whether the tiles are in a straight line </returns>
+    public bool GetStraightDirectionBetweenTiles(MapTile startTile, MapTile endTile, out StraightDirection direction) {
+        var xDiff = endTile.Position.x - startTile.Position.x;
+        var yDiff = endTile.Position.y - startTile.Position.y;
+
+        if (xDiff == 0 && yDiff == 0) {
+            direction = StraightDirection.Up;
+            return false;
+        }
+
+        if (xDiff == 0) {
+            direction = yDiff > 0 ? StraightDirection.Up : StraightDirection.Down;
+            return true;
+        }
+
+        if (yDiff == 0) {
+            direction = xDiff > 0 ? StraightDirection.Right : StraightDirection.Left;
+            return true;
+        }
+
+        direction = StraightDirection.Up;
+        return false;
+    }
+
+    /// <summary>
+    ///     Get tiles in a straight line from a tile in a direction
+    /// </summary>
+    /// <param name="fromTile"> The tile to start from, excluded from the result </param>
+    /// <param name="direction"> The direction to go in </param>
+    /// <param name="distance"> The number of tiles to get </param>
+    /// <param name="reachedOverEdge"> Whether the end of the map was reached, and the resulting list is shorter than the distance </param>
+    /// <returns> All tiles in the direction from the start tile, excluding the start tile </returns>
+    /// <exception cref="ArgumentOutOfRangeException"> If the direction enum is not a straight direction </exception>
+    public List<MapTile> GetTilesInDirection(MapTile fromTile, StraightDirection direction, int distance, out bool reachedOverEdge) {
+        reachedOverEdge = false;
+        if (distance <= 0) return new List<MapTile>();
+        var result = new List<MapTile>();
+        var x = fromTile.Position.x;
+        var y = fromTile.Position.y;
+        for (var i = 0; i < distance; i++) {
+            switch (direction) {
+                case StraightDirection.Up:
+                    y++;
+                    break;
+                case StraightDirection.Down:
+                    y--;
+                    break;
+                case StraightDirection.Left:
+                    x--;
+                    break;
+                case StraightDirection.Right:
+                    x++;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+            }
+
+            if (x < 0 || x >= Width || y < 0 || y >= Height) {
+                reachedOverEdge = true;
+                break;
+            }
+
+            result.Add(_tiles[x, y]);
+        }
+
+        return result;
+    }
+
     public List<MapTile> GetTilesInRange(MapTile fromTile, int distance, Func<MapTile, bool> predicate = null, bool includeFromTile = false) {
         var result = new List<MapTile>();
         for (int x = 0; x < Width; x++) {
@@ -126,4 +201,11 @@ public class BattleMap : IInstance {
             Height = height;
         }
     }
+}
+
+public enum StraightDirection {
+    Up,
+    Down,
+    Left,
+    Right
 }
